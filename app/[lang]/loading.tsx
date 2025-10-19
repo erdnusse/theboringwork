ee5ade0ed5930f98c9a9923650c2d2c05141405f
erdnusse <marco.damiao@gmail.com>
2025-10-19 20:39:59 +0100

    Implement loading spinner in Loading component

--- app/[lang]/loading.tsx
+++ app/[lang]/loading.tsx
@@ -1,3 +1,7 @@
+import ModernSpinner from "@/components/loading-spinner";
+
 export default function Loading() {
-  return null
+  return (  <div className="w-full h-full z-0 flex items-center justify-center">
+            <ModernSpinner />
+          </div>);
 }
